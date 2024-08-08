bc88c82de9b19054ac9bb8e47e8c3aa5ff703095
dhysoris <152842776+dhysoris@users.noreply.github.com>
2024-08-08 16:59:51 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+import {  gridImage1, gridImage2, gridImage3 } from "./images.js"
+
+gsap.registerPlugin(ScrollTrigger)
+
+const lenis = new Lenis({
+    scrollTrigger: {
+        lerp: 0.1,
+        smoothWheel: true
+    }
+})
+
+lenis.on("scroll", ScrollTrigger.update)
+gsap.ticker.add((timer) => lenis.raf(timer * 400))
+gsap.ticker.lagSmoothing[0]
+
+const paragraphs = document.querySelectorAll("p")
+let span = []
+
+paragraphs.forEach(paragraph => {
+    let html = ""
+    let pArray = paragraph.textContent.split("")
+    pArray.map(array => html += `<span>${array}</span>`)
+    paragraph.innerHTML = html
+});
+
+span = [...document.querySelectorAll("span")]
+
+function animateText () {
+    span.forEach(span => {
+        if (span.parentElement.getBoundingClientRect().top < window.innerHeight / 2) {
+            let {left, top} = span.getBoundingClientRect();
+            top = top - (window.innerHeight * .4)
+            let opacityValue = 1 - ((top * .01) + (left * .001)) < 0.1 ? 0.1 : 1 - ((top * .01) + (left * .001)).toFixed(3)
+            span.style.opacity = opacityValue
+        }
+    })
+}
+
+
+const left = document.querySelector(".left")
+const right = document.querySelector(".right")
+const footer = document.querySelector("footer")
+
+function animateFooter () {
+    let {bottom} = footer.getBoundingClientRect();
+    let scale = (bottom - window.innerHeight)
+    scale = scale < 0 ? 0 : scale 
+    left.style.transform = `translateX(${-scale}px)`
+    right.style.transform = `translateX(${scale}px)`
+}
+
+window.addEventListener("scroll", () => {
+    animateText()
+    animateFooter()
+})
+
+
+
+
---
+++ masonry.js
@@ -0,0 +1,30 @@
+
+import { gridImage1, gridImage2, gridImage3 } from "./images.js"
+
+
+const grid = document.querySelector(".masonry__grid")
+const gridFlow = document.querySelector(".grid__flow")
+
+function masonry () {
+    const dataset = [ gridImage1, gridImage2, gridImage3 ]
+
+    dataset.forEach(data => {
+        let gridContainer = document.createElement("div")
+        gridContainer.classList.add("grid__container")
+
+        data.forEach(set => {
+            let gridImagesContainer = document.createElement("div")
+            gridImagesContainer.classList.add("grid__image__container")
+
+            let gridImages = document.createElement("img")
+            gridImages.classList.add("grid__image")
+            gridImages.src = set.image
+
+            gridImagesContainer.appendChild(gridImages)
+            gridContainer.appendChild(gridImagesContainer)
+            gridFlow.appendChild(gridContainer)
+        })
+    })
+}
+
+masonry()
