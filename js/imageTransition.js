8721d066c7658fe7795bfb3ec21728eab77581a0
dhysoris <152842776+dhysoris@users.noreply.github.com>
2024-08-12 20:51:36 +0800

    Create imageTransition.js

---
+++ js/imageTransition.js
@@ -0,0 +1,30 @@
+import {  gridImage1, gridImage2, gridImage3 } from "./images.js"
+
+const pixelColor = document.querySelectorAll(".image__pixel__grid")
+const gridImages = document.querySelectorAll(".grid__image")
+const gridImageContainer = document.querySelectorAll(".grid__image__container")
+
+const dataset = [gridImage1, gridImage2, gridImage3]
+const container = document.querySelectorAll('.grid__image__container');
+
+container.forEach((container, index) => {
+    let arrayIndex = Math.floor(index / 5);
+    let objectIndex = index % 5;
+
+    const imageElement = container.querySelector('.grid__image');
+    imageElement.src = dataset[arrayIndex][objectIndex].image;
+    let showingOriginal = true;
+
+    container.addEventListener('click', function(e) {
+
+        if (showingOriginal) {
+            imageElement.src = dataset[arrayIndex][objectIndex].image2
+        } else {
+            imageElement.src = dataset[arrayIndex][objectIndex].image
+        }
+
+        showingOriginal = !showingOriginal
+        e.stopPropagation()
+
+    });
+});
