9e12e63fdffd2e38fe537f4fbd7d6bb7c5f7efe3
dhysoris <152842776+dhysoris@users.noreply.github.com>
2024-08-17 02:12:39 +0800

    Update images.js

--- js/images.js
+++ js/images.js
@@ -1,18 +1,23 @@
 const gridImage1 = [
 {
     image: "images/1.webp",
+    image: "images/b-1.webp",
 },
 {
     image: "images/2.webp",
+    image: "images/b-2.webp",
 },
 {
     image: "images/3.webp",
+    image: "images/b-3.webp",
 },
 {
     image: "images/4.webp",
+    image: "images/b-4.webp",
 },
 {
     image: "images/5.webp",
+    image: "images/b-5.webp",
 },
 ]
 
@@ -20,40 +25,49 @@ const gridImage1 = [
 let gridImage2 = [
  
 
-    {
-        image: "images/11.webp",
-    },
+{
+    image: "images/11.webp",
+    image: "images/b-6.webp",
+},
 {
     image: "images/6.webp",
+    image: "images/b-7.webp",
 },
 {
     image: "images/7.webp",
+    image: "images/b-8.png",
 },
 {
     image: "images/16.png",
+    image: "images/b-9.webp",
 },
-
 {
     image: "images/10.webp",
+    image: "images/b-10.webp",
 },
 ]
 
 
 let gridImage3 = [
-    {
-        image: "images/17.png",
-    },
+{
+    image: "images/17.png",
+    image: "images/b-11.webp",
+},
 {
     image: "images/12.webp",
+    image: "images/b-12.webp",
 },
 {
     image: "images/13.webp",
+    image: "images/b-13.webp",
 },
 {
     image: "images/14.webp",
+    image: "images/b-14.webp",
 },
 {
     image: "images/15.webp",
+    image: "images/b-15.webp",
 },
 ]
 
