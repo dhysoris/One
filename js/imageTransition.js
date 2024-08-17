eb2e229d57ba2ce39f6783a4819ea4dbd3abfdc6
dhysoris <152842776+dhysoris@users.noreply.github.com>
2024-08-17 19:19:21 +0800

    Update imageTransition.js

--- js/imageTransition.js
+++ js/imageTransition.js
@@ -4,12 +4,13 @@ const pixelColor = document.querySelectorAll(".image__pixel__grid")
 const gridImages = document.querySelectorAll(".grid__image")
 const gridImageContainer = document.querySelectorAll(".grid__image__container")
 
+
 const dataset = [gridImage1, gridImage2, gridImage3]
 const container = document.querySelectorAll('.grid__image__container');
 
 container.forEach((container, index) => {
-    let arrayIndex = Math.floor(index / 5);
-    let objectIndex = index % 5;
+    let arrayIndex = Math.floor(index / 5); // Determine which array to use
+    let objectIndex = index % 5; // Determine which object in the array
 
     const imageElement = container.querySelector('.grid__image');
     imageElement.src = dataset[arrayIndex][objectIndex].image;
@@ -25,6 +26,30 @@ container.forEach((container, index) => {
 
         showingOriginal = !showingOriginal
         e.stopPropagation()
-
+        
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
